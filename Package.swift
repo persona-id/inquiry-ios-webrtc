824917ab0bb22cf358567b5f6650275ce680f5bf
persona-identities <57889169+persona-identities@users.noreply.github.com>
2024-04-03 10:50:45 -0700

    Update Package.swift for 2.17.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "PersonaWebRtc",
       url: "https://github.com/persona-id/inquiry-ios-webrtc/releases/download/2.17.1/PersonaWebRtc.xcframework.zip",
-      checksum: "441addd1424cfb7c9ba298d30f989a5abbc911323ee088283394e0a2f351544b"
+      checksum: "78e9e1105e7d086bf55afe4bc36f1c1eaac604319ac6a41dfdd27c34be5ba543"
     )
   ]
 )
