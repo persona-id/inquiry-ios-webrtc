fc246225b52434f357f1a2ed8042be1de3337644
Ariel-Liao <46766415+Ariel-Liao@users.noreply.github.com>
2024-04-16 18:05:37 -0400

    Update Package.swift for 2.19.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "PersonaWebRtc",
       url: "https://github.com/persona-id/inquiry-ios-webrtc/releases/download/2.19.1/PersonaWebRtc.xcframework.zip",
-      checksum: "8939450bec8b3c75626dfdc3ffdb1df823c1c94532817d6282e5a1f5e392c76d"
+      checksum: "fc9a67454fc8eebd4d8e406a088f24f5062394726525e165d751ab1c2e4b963f"
     )
   ]
 )
