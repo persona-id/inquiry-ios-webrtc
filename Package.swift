558bfd70393686729d7397ee2651f8b6d41d877f
mobile-build-persona <mobile-build@withpersona.com>
2025-05-15 21:43:05 -0500

    Update Package.swift for 2.26.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "PersonaWebRtc",
       url: "https://github.com/persona-id/inquiry-ios-webrtc/releases/download/2.26.2/PersonaWebRtc.xcframework.zip",
-      checksum: "d3dd3ff58daa582fa4249d4aba3905b07d3b5fc612b362eda2d4d75fc1c8e542"
+      checksum: "a3e1f8d8f20eb8213a97e77c1f6ce4d3ef3634cfd33c1f0ece51887d538d62c6"
     )
   ]
 )
