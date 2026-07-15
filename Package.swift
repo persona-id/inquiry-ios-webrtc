4c1e13f800c6a011206001e51f3fa8fea55d4d1a
mobile-build-persona <mobile-build@withpersona.com>
2026-07-15 15:32:51 -0600

    Bump to 2.51.1-beta.2

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaWebRtc",
-      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/2.51.1-beta.1/PersonaWebRtc.xcframework.zip",
-      checksum: "30929206861cac93c2d0937f6bf36fd4de56b4d05a1f85c164029f508af96fa6"
+      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/2.51.1-beta.2/PersonaWebRtc.xcframework.zip",
+      checksum: "4eaa1bdf16f9b1b077869b930a4c0942b11f6e49aad343e3cac34b220066f545"
     )
   ]
 )
